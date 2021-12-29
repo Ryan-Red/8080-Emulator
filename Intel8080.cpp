b9e0e24258ffe890b28ecc8f3bbfe7406c826521
Ryan <ryan_red@outlook.com>
2021-12-29 18:38:25 +0100

    Added some of the op codes required for space invaders

--- Intel8080.cpp
+++ Intel8080.cpp
@@ -1,16 +1,28 @@
 #include "Intel8080.h"
 
-
-Intel8080::Intel8080(char* gameDir)
+Intel8080::Intel8080(char *gameDir)
 {
     m_gameDir = gameDir;
     ResetState();
     GenerateBuffer();
-
-
 }
 
-void Intel8080::ResetState(){
+uint8_t Intel8080::Parity(uint8_t val)
+{
+
+    int counter;
+    int i = 0;
+
+    for (i = 0; i < 16; i++)
+    {
+        counter += (val >> i) & 0x1;
+    }
+
+    return (counter % 2) == 0 ? 1 : 0;
+}
+
+void Intel8080::ResetState()
+{
     m_state.a = 0;
     m_state.b = 0;
     m_state.c = 0;
@@ -25,92 +37,143 @@ void Intel8080::ResetState(){
     m_state.int_enable = 0; // To be changed later on when starting the emulator ig.
 
     //Reset the ConditionCodes
-    m_state.cc.z= 0;
+    m_state.cc.z = 0;
     m_state.cc.s = 0;
     m_state.cc.p = 0;
     m_state.cc.pad = 0;
     m_state.cc.cy = 0;
     m_state.cc.ac = 0;
-
 }
 
+void Intel8080::GenerateBuffer()
+{
 
-void Intel8080::GenerateBuffer(){
+    FILE *f = fopen(m_gameDir, "rb");
 
-    FILE *f= fopen(m_gameDir, "rb"); 
+    if (f == NULL)
+    {
+        std::cout << "error: Couldn't open " << m_gameDir << std::endl;
+    }
 
-    if (f==NULL)    
-    {    
-        std::cout << "error: Couldn't open " << m_gameDir << std::endl;    
-    }    
+    //Get the file size and read it into a memory buffer
+    fseek(f, 0L, SEEK_END);
+    int fsize = ftell(f);
+    m_gameBufferLen = fsize;
 
-    //Get the file size and read it into a memory buffer    
-    fseek(f, 0L, SEEK_END);    
-    int fsize = ftell(f);  
-    m_gameBufferLen = fsize;  
+    fseek(f, 0L, SEEK_SET);
 
-    fseek(f, 0L, SEEK_SET);    
+    m_gameBuffer = (unsigned char *)malloc(fsize);
 
-    m_gameBuffer = (unsigned char*) malloc(fsize);
-
-    fread(m_gameBuffer, fsize, 1, f);    
-    fclose(f);    
-
-
-
-    
+    fread(m_gameBuffer, fsize, 1, f);
+    fclose(f);
 }
 
- void Intel8080::UnimplementedInstruction(unsigned char* instruction ){
-    //pc will have advanced one, so undo that   
+void Intel8080::UnimplementedInstruction(unsigned char *instruction)
+{
+    //pc will have advanced one, so undo that
     printf("Error: Instruction $#%02x not yet implemented\n", m_gameBuffer[m_state.pc]);
-    exit(1);   
- }
+    exit(1);
+}
 
- int Intel8080::run(){
+int Intel8080::run()
+{
 
-    while(m_state.pc < m_gameBufferLen){
-    
+    while (m_state.pc < m_gameBufferLen)
+    {
+
         Emulate8080OpCode();
-
     }
+}
 
+int Intel8080::Emulate8080OpCode()
+{
 
- }
- int Intel8080::Emulate8080OpCode(){
+    unsigned char *opcode = &m_state.memory[m_state.pc];
 
-    unsigned char* opcode = &m_state.memory[m_state.pc];
+    switch (*opcode)
+    {
 
-    switch(*opcode){
-
-        case 0x00: printf("NOP"); break;
-        case 0x01:{ //LXI B,D16         B <- byte 3, C <- byte 2
-            m_state.b = opcode[2];
-            m_state.c = opcode[1];
-            m_state.pc +=2;
-            break;
-
-        }
-        case 0x02:{
-            m_state.memory[(m_state.b << 8) | (m_state.c) ] = m_state.a;
-
-
-        } printf("STAX B"); break;
-        case 0x03: printf("INX  B"); break;
-        case 0x04: printf("INR  B"); break;
-        case 0x05: printf("DCR  B"); break;
-        case 0x06: printf("MVI  B,#$%02x",code[1]); opbyte = 2; break;
-        case 0x07: printf("RLC"); break;
-
-        default:{ 
-            UnimplementedInstruction(opcode[0]);
-        }
-        
+    case 0x00:
+        printf("NOP");
+        break;
+    case 0x01:
+    { //LXI B,D16         B <- byte 3, C <- byte 2
+        m_state.b = opcode[2];
+        m_state.c = opcode[1];
+        m_state.pc += 2;
+        break;
     }
+    case 0x02:
+    {
+        m_state.memory[(m_state.b << 8) | (m_state.c)] = m_state.a;
+        break;
+    }
+    case 0x03:
+        printf("INX  B");
+        break;
+    case 0x04:
+        printf("INR  B");
+        break;
+    case 0x05:
+        m_state.b = m_state.b - 1;
 
+        m_state.cc.z = (m_state.b & 0xFF) == 0;  //Zero flag
+        m_state.cc.s = (m_state.b & 0x80) == 1;  // Sign flag
+        m_state.cc.ac = (m_state.b < 0xF) == 1;  //Aux Carry Flag
+        m_state.cc.p = Parity(m_state.b & 0xFF); //Parity
 
+        break;
 
+    case 0x06:
+        m_state.b = opcode[1];
+        break;
 
+    case 0x09:
+        uint8_t temp = ((m_state.h << 8) | (m_state.l)) + ((m_state.b << 8) | (m_state.c));
+        m_state.h = (temp >> 8) & 0xFF;
+        m_state.l = temp & 0xFF;
 
+        m_state.cc.cy = (temp > 0xFF); //Carry flag
+        break;
 
- }
+    case 0x0d:
+        m_state.c = m_state.c - 1;
+
+        m_state.cc.z = (m_state.c & 0xFF) == 0;  //Zero flag
+        m_state.cc.s = (m_state.c & 0x80) == 1;  // Sign flag
+        m_state.cc.ac = (m_state.c < 0xF) == 1;  //Aux Carry Flag
+        m_state.cc.p = Parity(m_state.c & 0xFF); //Parity
+        break;
+
+    case 0x0e:
+        m_state.c = opcode[1];
+        break;
+
+    case 0x0f:
+        int8_t tempA = m_state.a;
+        int8_t prev0 = tempA & 0x01;
+
+        m_state.a = (m_state.a >> 1) & ((prev0 << 7) | 0x7F);
+        m_state.cc.cy = prev0;
+        break;
+
+    case 0x11:
+        m_state.d = opcode[2];
+        m_state.e = opcode[1];
+        m_state.pc += 2;
+        break;
+
+    default:
+    {
+        UnimplementedInstruction(opcode);
+    }
+    }
+    m_state.pc += 1;
+}
+
+int main()
+{
+    int wow = Parity(26);
+    std::cout << wow;
+    return 0;
+}
--- Intel8080.h
+++ Intel8080.h
@@ -6,15 +6,16 @@
 
 typedef struct ConditionCodes //Condition flags of the 8080. Gets changed after some opcodes
 {
-    uint8_t z:1;
-    uint8_t s:1;
-    uint8_t p:1;
-    uint8_t cy:1;
-    uint8_t ac:1;
-    uint8_t pad:3;
+    uint8_t z : 1;
+    uint8_t s : 1;
+    uint8_t p : 1;
+    uint8_t cy : 1;
+    uint8_t ac : 1;
+    uint8_t pad : 3;
 } ConditionCodes;
 
-typedef struct State8080{ //All registers, pointers etc in the 8080
+typedef struct State8080
+{ //All registers, pointers etc in the 8080
 
     uint8_t a;
     uint8_t b;
@@ -36,30 +37,27 @@ class Intel8080
 private:
     State8080 m_state; //m_state of emulator
 
-    char* m_gameDir; //Directory where the game file is located
+    char *m_gameDir; //Directory where the game file is located
 
-    unsigned char* m_gameBuffer; //buffer that has all the of the game buffer.
+    unsigned char *m_gameBuffer; //buffer that has all the of the game buffer.
     int m_gameBufferLen;
 
-    void UnimplementedInstruction(unsigned char* instruction); // Instruction not implemented, use this to remind myself what still needs to be done
+    void UnimplementedInstruction(unsigned char *instruction); // Instruction not implemented, use this to remind myself what still needs to be done
 
     void GenerateBuffer(); //Generate the game buffer to be used to emulate the game
-    
+
     void ResetState(); // reinitialize the m_state of the 8080
 
 public:
-    Intel8080(char* gameDir);
+    Intel8080(char *gameDir);
     ~Intel8080();
 
     int Emulate8080OpCode(); //Run the emulator
 
     int run();
-
-
+    uint8_t Parity(uint8_t val);
 };
-
 
 Intel8080::~Intel8080()
 {
 }
-
